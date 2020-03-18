133dfeff97f6e4766dee7352fcf6761078518f47
olayenca <o.otuniyi@elsevier.com>
2020-03-18 09:54:52 +0000

    EPDF-2605 bash permissions

--- merge_script.sh
+++ merge_script.sh
@@ -1,7 +1,7 @@
 export GIT_COMMITTER_EMAIL="o.otuniyi@elsevier.com"
-export GIT_COMMITTER_NAME="olayenca"
+export GIT_COMMITTER_NAME="Olayinka Otuniyi"
 git config --add remote.origin.fetch +refs/heads/*:refs/remotes/origin/* || exit
 git fetch --all || exit
 git checkout master || exit
 git merge --no-ff "$TRAVIS_COMMIT" || exit
-git push @github.com/">https://${GITHUB_TOKEN}@github.com/olayenca/react-logic-switch.git
+git push @github.com/">https://${GITHUB_TOKEN}@github.com/olayenca/react-logic-switch.git"
