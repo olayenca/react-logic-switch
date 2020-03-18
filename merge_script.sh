411067be9b52a602ea23e7ea4292ca286cd85d29
olayenca <o.otuniyi@elsevier.com>
2020-03-18 14:04:21 +0000

    EPDF-2607 secure

--- merge_script.sh
+++ merge_script.sh
@@ -14,6 +14,3 @@ git merge --ff-only "$TRAVIS_COMMIT" || exit
 printf '3>>>>>>>>>\n'
 git push https://${GITHUB_SECRET_TOKEN}@github.com/olayenca/react-logic-switch.git
 printf '4>>>>>>>>>>>>n'
-
-
-travis encrypt 'GITHUB_NAME="olayenca"  GITHUB_EMAIL="o.otuniyi@elsevier.com" GITHUB_SECRET_TOKEN=5060ff47aa0c1f26c1e84e7ec2a22a93e3fa2e1b'
