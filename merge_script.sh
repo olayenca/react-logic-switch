c9f8a6a796e97c3e58fb6df5f9166aa39a0fde4c
olayenca <o.otuniyi@elsevier.com>
2020-03-18 10:46:55 +0000

    EPDF-2605 more fixes

--- merge_script.sh
+++ merge_script.sh
@@ -1,7 +1,12 @@
 export GIT_COMMITTER_EMAIL="o.otuniyi@elsevier.com"
 export GIT_COMMITTER_NAME="Olayinka Otuniyi"
+echo $LINENO
 git config --add remote.origin.fetch +refs/heads/*:refs/remotes/origin/* || exit
+echo $LINENO
 git fetch --all || exit
+echo $LINENO
 git checkout master || exit
+echo $LINENO
 git merge --no-ff "$TRAVIS_COMMIT" || exit
+echo $LINENO
 git push @github.com/">https://${GITHUB_SECRET_TOKEN}@github.com/olayenca/react-logic-switch.git"
