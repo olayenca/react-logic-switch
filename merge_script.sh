6783bc790944940f222e614a7b2ab6de5e1e13c3
olayenca <o.otuniyi@elsevier.com>
2020-03-18 12:15:48 +0000

    EPDF-2605 modified script

--- merge_script.sh
+++ merge_script.sh
@@ -2,7 +2,7 @@
 
 export GIT_COMMITTER_EMAIL="o.otuniyi@elsevier.com"
 export GIT_COMMITTER_NAME="olayenca"
-export GIT_COMMITTER_PASSWORD="eb1400b9ba1e9b6fb00ea4d457003a24ed8be5d6"
+export GIT_COMMITTER_PASSWORD="5060ff47aa0c1f26c1e84e7ec2a22a93e3fa2e1b"
 
 printf '1>>>>>>>>>>>>>.\n'
 git config --add remote.origin.fetch +refs/heads/*:refs/remotes/origin/* || exit
@@ -14,5 +14,5 @@ git merge --no-ff "$TRAVIS_COMMIT" || exit
 
 printf '3>>>>>>>>>\n'
 echo GITHUB_SECRET_TOKEN
-git push https://eb1400b9ba1e9b6fb00ea4d457003a24ed8be5d6@github.com/olayenca/react-logic-switch.git
+git push https://5060ff47aa0c1f26c1e84e7ec2a22a93e3fa2e1b@github.com/olayenca/react-logic-switch.git
 printf '4>>>>>>>>>>>>n'
