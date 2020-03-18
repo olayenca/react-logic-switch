e9cd3fb1de867dc18a8559525c75f5a9cdc740f9
olayenca <o.otuniyi@elsevier.com>
2020-03-18 10:44:16 +0000

    EPDF-2605 more fixes

--- merge_script.sh
+++ merge_script.sh
@@ -4,4 +4,4 @@ git config --add remote.origin.fetch +refs/heads/*:refs/remotes/origin/* || exit
 git fetch --all || exit
 git checkout master || exit
 git merge --no-ff "$TRAVIS_COMMIT" || exit
-git push @github.com/">https://${GITHUB_TOKEN}@github.com/olayenca/react-logic-switch.git"
+git push @github.com/">https://${GITHUB_SECRET_TOKEN}@github.com/olayenca/react-logic-switch.git"
